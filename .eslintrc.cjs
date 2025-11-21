d6dfd59176a611477517651687ffa4be66fdb4d4
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2025-11-21 18:15:02 +0530

    Enhance ESLint configuration with comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  root: true,                          // This tells ESLint that this is the root config (no parent configs above)
+  env: { browser: true, es2020: true }, // Defines environment: browser globals + ES2020 features available
+
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'eslint:recommended',               // Starts with ESLint's recommended default rules
+    'plugin:@typescript-eslint/recommended', // Adds recommended TypeScript rules
+    'plugin:react-hooks/recommended',   // Adds recommended rules for React Hooks usage
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+
+  ignorePatterns: ['dist', '.eslintrc.cjs'], // ESLint will ignore these files/folders
+
+  parser: '@typescript-eslint/parser',  // Uses the TS parser so ESLint understands TypeScript code
+
+  plugins: ['react-refresh'],           // Adds React Refresh plugin (helps with fast refresh in dev)
+
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+      'warn',                           // Show warning (not error) for rule violations
+      { allowConstantExport: true },    // Allow components exported as constants
     ],
   },
 }
+
